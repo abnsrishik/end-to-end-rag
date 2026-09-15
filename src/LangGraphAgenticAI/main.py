26a1f2090038de574eca6fbf478ff5d78b316381
Atkuri Bala Naga Sai Narasimha Subramanya Rishik <rishikcr72401@gmail.com>
2026-09-15 11:46:24 +0530

    Updated: added a condition for Button fuction - Fetch News

--- src/LangGraphAgenticAI/main.py
+++ src/LangGraphAgenticAI/main.py
@@ -20,7 +20,10 @@ def load_langgraph_agentic_app():
         st.error("Error: Failed to load user input from the UI.")
         return 
     
-    user_message = st.chat_input("Enter your Message")
+    if st.session_state.IsFetchButtonClicked:
+        user_message = st.session_state.time_frame
+    else:
+        user_message = st.chat_input("Enter your message: ")
 
     if user_message:
         try:
